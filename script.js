894a5adbf3cdb733be233bf201858063f6cb4ccb
ManuelBeckerT <mabecker@uc.cl>
2021-02-26 17:35:26 -0300

    Count down added

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ onReady(function () {
     show('page', true);
     show('loading', false);
 });
+
+
+function startTimer(duration, display) {
+    var timer = duration, hours, minutes, seconds;
+    var timerId = setInterval(function () {
+
+
+        hours = Math.floor((timer / 3600) % 24);
+        minutes = Math.floor((timer / 60) % 60);
+        seconds = parseInt(timer % 60, 10);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = hours + "h " + minutes + "m " + seconds + "s";
+
+
+        if (--timer < 0) {
+            clearInterval(timerId);
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var countDownDate = new Date("Feb 26, 2021 17:35:00").getTime();
+    var currentDate = new Date();
+    var currentTime = currentDate.getTime();
+    var time = (countDownDate - currentTime) / 1000;  // Tiempo en segundos
+    display = document.querySelector('#time')
+    startTimer(time, display);
+};
+
+
+
